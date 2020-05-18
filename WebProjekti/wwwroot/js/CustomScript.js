ce14f1c250d1d7f27c4adc32afbca6dc6f04c0c2
Besar Kutleshi <besar.kutleshi0@gmail.com>
2020-05-18 19:47:28 +0200

    Account Layout completed

--- WebProjekti/wwwroot/js/CustomScript.js
+++ WebProjekti/wwwroot/js/CustomScript.js
@@ -10,3 +10,15 @@
         $('#' + confirmDeleteSpan).hide();
     }
 }
+
+$(document).ready(function () {
+    $("#sidebar").mCustomScrollbar({
+        theme: "minimal"
+    });
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar, #content').toggleClass('active');
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+});
